909ed62dd8ccb63490e24fbe24219a46a5d3fa68
adeafblindman <adeafblindman@gmail.com>
2017-12-20 15:47:37 -0600

    removed indents

--- weatherbot.py
+++ weatherbot.py
@@ -1,14 +1,14 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import bot
-import asyncio
+
 
 
 bot = commands.Bot(command_prefix='!')
 
 
 @bot.event
-async def on_ready():
+def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -20,17 +20,17 @@ async def on_ready():
 
 
 @bot.command(pass_context=True)
-async def ping(ctx):
+def ping(ctx):
 	await bot.say("im alive!")
 
 
 @bot.command(pass_context=True)
-async def w(ctx):
+def w(ctx):
     embed = discord.Embed(title="title", description="description",color=0xDF4D11)
     embed.set_footer(text="footer")
     embed.set_author(name="Author")
     embed.add_field(name="Weather Coming Soon", value="value", inline=True)
-    await bot.say(embed=w)
+     await bot.send_message(ctx.message.channel, embed=w)
 #---------
 #bot TOKEN
 #---------
