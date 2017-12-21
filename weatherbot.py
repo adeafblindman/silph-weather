caabf609baaa0991c565f8fcaa780eb696c07707
Jacob Boline <mail@jboline.us>
2017-12-20 20:00:50 -0600

    More weather conditions.

--- weatherbot.py
+++ weatherbot.py
@@ -14,6 +14,15 @@ async def on_ready():
     print('-----RUNNING-----')
 
 #-------------
+#utility functions
+#-------------
+def degToCompass(num):
+    val=int((num/22.5)+.5)
+    arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
+    return arr[(val % 16)]
+
+
+#-------------
 #bot commands
 #-------------
 
@@ -29,23 +38,32 @@ async def w(ctx, *args):
     ## Join all the strings given.
     location = ' '.join(args)
 
-    ## Get the weather
-    observation = owm.weather_at_place(location)
-    w = observation.get_weather()
+    try:
+        ## Get the weather
+        observation = owm.weather_at_place(location)
+        w = observation.get_weather()
 
-    sky_status = w.get_status()
-    temp_f = w.get_temperature(unit = 'fahrenheit')
-    wind = w.get_wind()
-    wind_mph = round(wind["speed"] * 2.23694, 2)
+        sky_status = w.get_status()
+        temp_f     = w.get_temperature(unit = 'fahrenheit')
+        wind       = w.get_wind()
+        wind_mph   = round(wind["speed"] * 2.23694, 2)
+        cloud_cov  = w.get_clouds()
+        humidity   = w.get_humidity()
+        wx_code    = w.get_weather_code()
 
-    ## Print the weather.
-    msg = discord.Embed(title= "Weather for " + str(location), description = str(sky_status), color=0xDF4D11)
-    msg.add_field(name="Temperature", value = str(temp_f["temp"]) + " F", inline=True)
-    msg.add_field(name="Wind", value = str(wind_mph) + " MPH", inline=True)
-    
+        ## Print the weather.
+        msg = discord.Embed(title= "Weather for " + str(location), description = str(sky_status), color=0xDF4D11)
+        msg.add_field(name = "Temperature", value = str(temp_f["temp"]) + " F", inline = False)
+        msg.add_field(name = "Wind", value =  degToCompass(wind["deg"]) + ' ' + str(wind_mph) + " MPH", inline = False)
+        msg.add_field(name = "Cloud Coverage", value =  str(cloud_cov) + " %", inline = False)
+        msg.add_field(name = "Humidity", value =  str(humidity) + " %", inline = False)
+        msg.add_field(name = "Code", value =  str(wx_code), inline = False)
 
-    await bot.send_message(ctx.message.channel, embed=msg)
-    #await bot.send_message(ctx.message.channel, str(location))
+
+        await bot.send_message(ctx.message.channel, embed = msg)
+    except pyowm.exceptions.not_found_error.NotFoundError:
+        await bot.say("Location not found!")
+
 
 #---------
 #bot TOKEN
