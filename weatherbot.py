86f7c32222525864cca8c757f6487fc3d09cc784
Jacob Boline <mail@jboline.us>
2017-12-20 19:26:16 -0600

    Add weather.

--- weatherbot.py
+++ weatherbot.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+import pyowm
 
 bot = commands.Bot(command_prefix='!')
+owm = pyowm.OWM('')
 
 
 @bot.event
@@ -22,14 +24,31 @@ async def ping(ctx):
 
 
 @bot.command(pass_context=True)
-async def w(ctx):
-    msg = discord.Embed(title="title", description="description",color=0xDF4D11)
-    msg.set_footer(text="footer")
-    msg.set_author(name="Author")
-    msg.add_field(name="Weather Coming Soon", value="value", inline=True)
+async def w(ctx, *args):
+
+    ## Join all the strings given.
+    location = ' '.join(args)
+
+    ## Get the weather
+    observation = owm.weather_at_place(location)
+    w = observation.get_weather()
+
+    sky_status = w.get_status()
+    temp_f = w.get_temperature(unit = 'fahrenheit')
+    wind = w.get_wind()
+    wind_mph = round(wind["speed"] * 2.23694, 2)
+
+    ## Print the weather.
+    msg = discord.Embed(title= "Weather for " + str(location), description = str(sky_status), color=0xDF4D11)
+    msg.add_field(name="Temperature", value = str(temp_f["temp"]) + " F", inline=True)
+    msg.add_field(name="Wind", value = str(wind_mph) + " MPH", inline=True)
+    
+
     await bot.send_message(ctx.message.channel, embed=msg)
+    #await bot.send_message(ctx.message.channel, str(location))
+
 #---------
 #bot TOKEN
 #---------
 
-bot.run('token')
+bot.run('')
